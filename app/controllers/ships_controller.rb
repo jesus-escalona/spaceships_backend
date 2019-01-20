7be5a4049eb61a2c0ca052323962ff27db31265e
github username <patcarrasco@outlook.com>
2019-01-20 18:31:23 -0500

    fixed merge conflict

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,10 +1,10 @@
 class ShipsController < ApplicationController
+
 	def index
 		@ships = Ship.all
 		render json: @ships
 	end
 
-<<<<<<< HEAD
 	def create
 		byebug
     ship = Ship.new(ship_params)
@@ -16,16 +16,15 @@ class ShipsController < ApplicationController
     end
   end
 
+	def show
+		@ship = Ship.find(params[:id])
+		render json: @ship
+	end
+
 	private
 
 	def ship_params
 		params.require(:ship).permit(:name, :color, :health)
 	end
 
-=======
-	def show
-		@ship = Ship.find(params[:id])
-		render json: @ship
-	end
->>>>>>> 5d84161b8f186b3e40c942fe8e359451b1d8020b
 end
