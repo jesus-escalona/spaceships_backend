885053a41e9935b9bfaf27275c08f27a97aedbba
github username <patcarrasco@outlook.com>
2019-01-20 15:41:34 -0500

    enabling front and back end communication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :resources
   resources :ships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # root to: 'ships#index'
+
+  mount ActionCable.server, at: '/cable'
+
+
 end
